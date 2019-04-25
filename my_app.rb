a18bbab5b2f772f85b0546dc93aec54e79450338
Jason Rhoads <jasrhoads@gmail.com>
2019-04-25 10:19:36 -0700

    successful loading of static pages

--- my_app.rb
+++ my_app.rb
@@ -5,64 +5,54 @@ class MyApp
   def call env
     req = Rack::Request.new(env)
     logger = Logger.new(STDOUT)
-    logger.level = Logger::WARN      
+    logger.level = Logger::WARN 
+    mime_types = {
+      ".css"  => "text/css",
+      ".html" => "text/html",
+      ".js"   => "application/javascript",
+      ".png"  => "image/png",
+      ".jpg"  => "image/jpeg",
+      ".jpeg" => "image/jpeg",
+    }
+    mime_types.default = "text/plain"
   
     root = File.expand_path(File.dirname(__FILE__)) + "/public"
-    index_file = root + "/index.html"
     success_file = root + "/success.html"
     failed_file = root + "/failed.html"
     doesnotexist_file = root + "/doesnotexist.html"
-
-    main_css = root + "/css/main.css"
-    success_css = root + "/css/success.css"
-    failed_css = root + "/css/failed.css"
-    
     
     if req.get?
-      # Does the requested file exist? If not return 404
-      # e.g. /foo/bar.css -> C:\MyDir\public\foo\bar.css
       file = root + req.path
-      puts file
-      #change '/' to append index.html to directory
       file += "/index.html" if File.directory?(file)
-      # What is the extension of the file? 
-      content = case File.extname(file)
-      # * if .css, return Content-Type text/css
-      when ".css"
-        "text/css"
-      # * if .html, return content-type text/html
-      when ".html"
-        "text/html"
-      # * if .js, return content-type application/javascript
-      when ".js"
-        "application/javascript"
-      # * if .png, return image contents and content-type image/png
-      when ".png"
-        "image/png"
-      when ".jpg"
-        "image/jpg"
-      # * if other, use content-type text/plain
-      else
-        "text/plain"
-      end
-               
+      content = mime_types[File.extname(file)]
+     
       if File.exist?(file)
         [200, {"Content-type" => "#{content}"}, [File.read(file, mode: "rb")]] 
+      elsif req.path == "/loogin" || req.path == "/loggin" || req.path == "/lgin"
+        [301, {"Location" => "/index.html"},[]]
       else
-        [404, {"Content-type" => "text/html"}, [File.read(doesnotexist_file), req.path]]
-      end    
-      # Examples
-      # browser requests /index.html, server sets content to text/html nd returns root + "/index.html" contents
-      #return [200, {"Content-type" => "#{content}"}, [File.read(index_file), File.read(main_css)]] if req.path == "/"
-      
-    elsif req.post?
+        [404, {"Content-type" => "text/html"}, [File.read(doesnotexist_file)]]
+      end
+
+    elsif req.post? && req.path =="/login"
       if ((req.params["username"] == "aladdin") && (req.params["password"] == "letmein"))
-        [200, {"Content-type" => "text/html"}, [File.read(success_file),File.read(success_css)]]  
+        [200, {"Content-type" => "text/html"}, [File.read(success_file)]]  
       else
-        [200, {"Content-type" => "text/html"}, [File.read(failed_file), File.read(failed_css)]]  
+        [200, {"Content-type" => "text/html"}, [File.read(failed_file)]]  
       end
     else
       [400, {"Content-type" => "text/plain"}, ["Sorry #{req.request_method}"]]
     end
+   
   end
+
+  def get_request_dispatcher(file, content)
+      if File.exist?(@file)
+        [200, {"Content-type" => "#{content}"}, [File.read(file, mode: "rb")]] 
+      elsif req.path == "/loogin" || req.path == "/loggin" || req.path == "/lgin"
+        [301, {"Location" => "/index.html"},[]]
+      else
+        [404, {"Content-type" => "text/html"}, [File.read(doesnotexist_file)]]
+      end
+    end
 end
