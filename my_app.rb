cbe354bc3e3d49936be35fa921f58d1b29b27d2a
Jason Rhoads <jasrhoads@gmail.com>
2019-05-28 11:12:33 -0700

    dynamic routing enabled
    added dynamic route table plus request_dispatcher to retrieve static assests.

--- my_app.rb
+++ my_app.rb
@@ -17,42 +17,53 @@ class MyApp
     mime_types.default = "text/plain"
   
     root = File.expand_path(File.dirname(__FILE__)) + "/public"
-    success_file = root + "/success.html"
-    failed_file = root + "/failed.html"
-    doesnotexist_file = root + "/doesnotexist.html"
+        
+    file = root + req.path
+    file += "/index.html" if File.directory?(file)
+    content = mime_types[File.extname(file)]
     
-    if req.get?
-      file = root + req.path
-      file += "/index.html" if File.directory?(file)
-      content = mime_types[File.extname(file)]
-     
-      if File.exist?(file)
-        [200, {"Content-type" => "#{content}"}, [File.read(file, mode: "rb")]] 
-      elsif req.path == "/loogin" || req.path == "/loggin" || req.path == "/lgin"
-        [301, {"Location" => "/index.html"},[]]
-      else
-        [404, {"Content-type" => "text/html"}, [File.read(doesnotexist_file)]]
-      end
+    routes = {
+      "POST /login" => Proc.new{ |req, root, mime_types| post_request_dispatcher(req, root, mime_types) },
+      "GET /me" => Proc.new{ |req, root, mime_types| get_request_dispatcher(req, root, mime_types) },
+    }
+    routes.default = Proc.new {|req, root, mime_types| request_dispatcher(req, root, mime_types)}
 
-    elsif req.post? && req.path =="/login"
-      if ((req.params["username"] == "aladdin") && (req.params["password"] == "letmein"))
-        [200, {"Content-type" => "text/html"}, [File.read(success_file)]]  
-      else
-        [200, {"Content-type" => "text/html"}, [File.read(failed_file)]]  
-      end
-    else
-      [400, {"Content-type" => "text/plain"}, ["Sorry #{req.request_method}"]]
-    end
-   
+    wanted = "#{req.request_method} #{req.path.downcase}"
+    routes[wanted].call(req, root, mime_types)
+ 
   end
 
-  def get_request_dispatcher(file, content)
-      if File.exist?(@file)
-        [200, {"Content-type" => "#{content}"}, [File.read(file, mode: "rb")]] 
-      elsif req.path == "/loogin" || req.path == "/loggin" || req.path == "/lgin"
-        [301, {"Location" => "/index.html"},[]]
+  def request_dispatcher(req, root, mime_types)
+      case req.request_method
+      when "GET"
+        get_request_dispatcher(req, root, mime_types)
+      when "POST"
+        post_request_dispatcher(req, root, mime_types)
       else
-        [404, {"Content-type" => "text/html"}, [File.read(doesnotexist_file)]]
+        [404, {"Content-type" => "text/html"}, [File.read("#{root}/doesnotexist.html")]]
       end
+  end
+
+  def get_request_dispatcher(req, root, mime_types)
+    file = root + req.path
+    file += "/index.html" if File.directory?(file)
+    if File.exist?(file)
+        [200, {"Content-type" => "#{mime_types[File.extname(file)]}"}, [File.read(file, mode: "rb")]] 
+      #elsif req.path == "/loogin" || req.path == "/loggin" || req.path == "/lgin"
+      #  [301, {"Location" => "/index.html"},[]]
+    else
+        [404, {"Content-type" => "text/html"}, [File.read("#{root}/doesnotexist.html")]]
     end
+  end
+
+  def post_request_dispatcher(req, root, mime_types)
+    username = req.params["username"]
+    password = req.params["password"]
+    if ((username == "aladdin") && (password == "letmein"))
+      [200, {"Content-type" => "text/html"}, [File.read("#{root}/success.html")]]  
+    else
+      [200, {"Content-type" => "text/html"}, [File.read("#{root}/failed.html")]]  
+    end
+  end
+
 end
