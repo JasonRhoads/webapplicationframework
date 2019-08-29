f01586a7bc316601302083fd001ad1a8960fe64b
Jason Rhoads <jasrhoads@gmail.com>
2019-08-29 13:23:48 -0700

    functions moved into seperate class files

--- clarity.rb
+++ clarity.rb
@@ -1,5 +1,9 @@
 require "rack"
 require "logger"
+require_relative "routes"
+require_relative "mime_types"
+require_relative "dynamic_request_dispatcher"
+require_relative "default_request_dispatcher"
 
 class Clarity
    def call env
@@ -11,67 +15,17 @@ class Clarity
     file = directory + request.path
     file += "/index.html" if File.directory?(file)
     
-    mime_types = get_mime_types(directory)
+    mime_types = MimeTypes.get_mime_types(directory)
     content = mime_types[File.extname(file)]    
-    routes = get_routes(directory)
+    routes = Routes.get_routes(directory)
             
     http_method_and_path = "#{request.request_method} #{request.path.downcase}"
     if routes.has_key? http_method_and_path
-      dynamic_request_dispatcher(routes[http_method_and_path], request, directory, mime_types)
+      DynamicRequestDispatcher.dynamic_request_dispatcher(routes[http_method_and_path], request, directory, mime_types)
     else
-      default_request_dispatcher(request, directory, mime_types)
+      DefaultRequestDispatcher.default_request_dispatcher(request, directory, mime_types)
     end
   end
-
-  def dynamic_request_dispatcher(controller_file_name, request, directory, mime_types)
-    *path_names, class_name, method_name = controller_file_name.scan(/(\w+\/|\w+)/).flatten
-    file_name = [path_names, class_name, ".rb"].flatten.join
-    class_name = class_name.split('_').map{|cn| cn.capitalize}.join
-    if File.exist?("#{directory}/../controllers/#{file_name}") == false
-      file_name = [path_names, class_name, ".rb"].flatten.join
-    end
-    load "#{directory}/../controllers/#{file_name}"
-    instance = Object.const_get(class_name).new
-    instance_method = instance.method(method_name)
-    response = instance_method.call(request, directory, mime_types)
-    @logger.debug %{class:#{class_name}, method:#{method_name}, status code:#{response[0]}}
-    response
-  end
-
-  def default_request_dispatcher(request, directory, mime_types)
-    file = directory + request.path
-    file += "/index.html" if File.directory?(file)
-    if File.exist?(file)
-        [200, {"Content-type" => "#{mime_types[File.extname(file)]}"}, [File.read(file, mode: "rb")]] 
-    else
-        [404, {"Content-type" => "text/html"}, [File.read("#{directory}/doesnotexist.html")]]
-    end
-  end
-
-  def get_routes(directory)
-    routes = {}
-    routes_file = File.open("#{directory}/../controllers/routes.txt")
-    routes_file.each do |line|
-      http_method_and_path, callback = line.scan(/(\w+\s+(?:\/\s|\/\w+)+)\s*(\w+#\w+)/)
-        .flatten
-      routes[http_method_and_path] = callback
-    end
-    routes
-  end
-
-  def get_mime_types(directory)
-    mime_types = {}
-    mime_types.default = "text/plain"
-    mime_types_file = File.open("#{directory}/../mime_types.txt")
-    mime_types_file.each do |line| 
-      content, file_extension = line.scan(/([a-z]+\/[a-z]+):\s+((?:\.[a-z]+\s+|\.[a-z]+)+)\b/)
-        .flatten
-      file_extension = file_extension.split(" ")
-      file_extension.each{|i| mime_types[i] = content}
-    end
-    mime_types
-  end
-
 end
 
 
