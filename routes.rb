efef1392e53d5fd0d5618a7cfd7f2b8ef78d3c45
Jason Rhoads <jasrhoads@gmail.com>
2019-08-29 13:02:47 -0700

    get_routes moved into seperate class and file

---
+++ routes.rb
@@ -0,0 +1,12 @@
+class Routes
+  def get_routes(directory)
+    routes = {}
+    routes_file = File.open("#{directory}/../controllers/routes.txt")
+    routes_file.each do |line|
+      http_method_and_path, callback = line.scan(/(\w+\s+(?:\/\s|\/\w+)+)\s*(\w+#\w+)/)
+        .flatten
+      routes[http_method_and_path] = callback
+    end
+    routes
+  end
+end
