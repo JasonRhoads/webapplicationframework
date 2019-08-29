ab68dfcf189571c0b0c1e48147341e3eee9742a1
Jason Rhoads <jasrhoads@gmail.com>
2019-08-29 12:58:34 -0700

    root renamed to directory
    CLARITY_ROOT renamed to CLAIRTY_DIRECTORY

--- clarity.rb
+++ clarity.rb
@@ -7,52 +7,50 @@ class Clarity
     @logger = Logger.new(STDOUT)
     @logger.level = Logger::DEBUG
     
-    root = ENV['CLARITY_ROOT'] ? ENV['CLARITY_ROOT'] : File.expand_path(File.dirname(__FILE__)) + "/public"    
-    file = root + request.path
+    directory = ENV['CLARITY_DIRECTORY'] ? ENV['CLARITY_DIRECTORY'] : File.expand_path(File.dirname(__FILE__)) + "/public"    
+    file = directory + request.path
     file += "/index.html" if File.directory?(file)
     
-    mime_types = get_mime_types(root)
+    mime_types = get_mime_types(directory)
     content = mime_types[File.extname(file)]    
-    routes = get_routes(root)
+    routes = get_routes(directory)
             
     http_method_and_path = "#{request.request_method} #{request.path.downcase}"
     if routes.has_key? http_method_and_path
-      dynamic_request_dispatcher(routes[http_method_and_path], request, root, mime_types)
+      dynamic_request_dispatcher(routes[http_method_and_path], request, directory, mime_types)
     else
-      default_request_dispatcher(request, root, mime_types)
+      default_request_dispatcher(request, directory, mime_types)
     end
   end
 
-  def dynamic_request_dispatcher(controller_file_name, request, root, mime_types)
+  def dynamic_request_dispatcher(controller_file_name, request, directory, mime_types)
     *path_names, class_name, method_name = controller_file_name.scan(/(\w+\/|\w+)/).flatten
     file_name = [path_names, class_name, ".rb"].flatten.join
     class_name = class_name.split('_').map{|cn| cn.capitalize}.join
-    puts file_name
-    if File.exist?("#{root}/../controllers/#{file_name}") == false
+    if File.exist?("#{directory}/../controllers/#{file_name}") == false
       file_name = [path_names, class_name, ".rb"].flatten.join
     end
-    puts file_name
-    load "#{root}/../controllers/#{file_name}"
+    load "#{directory}/../controllers/#{file_name}"
     instance = Object.const_get(class_name).new
     instance_method = instance.method(method_name)
-    response = instance_method.call(request, root, mime_types)
+    response = instance_method.call(request, directory, mime_types)
     @logger.debug %{class:#{class_name}, method:#{method_name}, status code:#{response[0]}}
     response
   end
 
-  def default_request_dispatcher(request, root, mime_types)
-    file = root + request.path
+  def default_request_dispatcher(request, directory, mime_types)
+    file = directory + request.path
     file += "/index.html" if File.directory?(file)
     if File.exist?(file)
         [200, {"Content-type" => "#{mime_types[File.extname(file)]}"}, [File.read(file, mode: "rb")]] 
     else
-        [404, {"Content-type" => "text/html"}, [File.read("#{root}/doesnotexist.html")]]
+        [404, {"Content-type" => "text/html"}, [File.read("#{directory}/doesnotexist.html")]]
     end
   end
 
-  def get_routes(root)
+  def get_routes(directory)
     routes = {}
-    routes_file = File.open("#{root}/../controllers/routes.txt")
+    routes_file = File.open("#{directory}/../controllers/routes.txt")
     routes_file.each do |line|
       http_method_and_path, callback = line.scan(/(\w+\s+(?:\/\s|\/\w+)+)\s*(\w+#\w+)/)
         .flatten
@@ -61,10 +59,10 @@ class Clarity
     routes
   end
 
-  def get_mime_types(root)
+  def get_mime_types(directory)
     mime_types = {}
     mime_types.default = "text/plain"
-    mime_types_file = File.open("#{root}/../mime_types.txt")
+    mime_types_file = File.open("#{directory}/../mime_types.txt")
     mime_types_file.each do |line| 
       content, file_extension = line.scan(/([a-z]+\/[a-z]+):\s+((?:\.[a-z]+\s+|\.[a-z]+)+)\b/)
         .flatten
