7d2728275ec81613bd6825201f47a19f490dc20c
Jason Rhoads <jasrhoads@gmail.com>
2019-09-19 16:18:05 -0700

    objects initialized

--- clarity.rb
+++ clarity.rb
@@ -2,8 +2,7 @@ require "rack"
 require "logger"
 require_relative "routes"
 require_relative "mime_types"
-require_relative "dynamic_request_dispatcher"
-require_relative "default_request_dispatcher"
+require_relative "request_dispatcher"
 
 class Clarity
    def call env
@@ -11,21 +10,20 @@ class Clarity
     @logger = Logger.new(STDOUT)
     @logger.level = Logger::DEBUG
     
-    directory = ENV['CLARITY_DIRECTORY'] ? ENV['CLARITY_DIRECTORY'] : File.expand_path(File.dirname(__FILE__)) + "/public"    
-    file = directory + request.path
+    app_root = ENV['CLARITY_DIRECTORY'] ? ENV['CLARITY_DIRECTORY'] : File.expand_path(File.dirname(__FILE__)) + "/public"    
+    file = app_root + request.path
     file += "/index.html" if File.directory?(file)
     
-    mime_types = MimeTypes.get_mime_types(directory)
-    content = mime_types[File.extname(file)]    
-    routes = Routes.get_routes(directory)
-            
+    mime_types = MimeTypes.new(app_root)
+    content = mime_types[File.extname(file)]
+    routes = Routes.new(app_root)
+    request_dispatcher = RequestDispatcher.new(app_root, mime_types, @logger)
+    
     http_method_and_path = "#{request.request_method} #{request.path.downcase}"
-    if routes.has_key? http_method_and_path
-      DynamicRequestDispatcher.dynamic_request_dispatcher(routes[http_method_and_path], request, directory, mime_types)
+    if routes.has_key?(http_method_and_path)
+      request_dispatcher.dynamic_request_dispatcher(routes[http_method_and_path], request)
     else
-      DefaultRequestDispatcher.default_request_dispatcher(request, directory, mime_types)
+      request_dispatcher.default_request_dispatcher(request)
     end
   end
 end
-
-
