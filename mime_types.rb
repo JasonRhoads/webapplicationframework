15aa2f0b7b0e4098ece564447ec6fb0a54dafcd9
Jason Rhoads <jasrhoads@gmail.com>
2019-09-19 16:16:48 -0700

    mime_types initialized into object

--- mime_types.rb
+++ mime_types.rb
@@ -1,14 +1,17 @@
 class MimeTypes
-  def self.get_mime_types(directory)
-    mime_types = {}
-    mime_types.default = "text/plain"
-    mime_types_file = File.open("#{directory}/../mime_types.txt")
-    mime_types_file.each do |line| 
+  def initialize(app_root)
+    @mime_types = {}
+    @mime_types.default = "text/plain"
+    @mime_types_file = File.open("#{app_root}/../mime_types.txt")
+    @mime_types_file.each do |line| 
       content, file_extension = line.scan(/([a-z]+\/[a-z]+):\s+((?:\.[a-z]+\s+|\.[a-z]+)+)\b/)
         .flatten
       file_extension = file_extension.split(" ")
-      file_extension.each{|i| mime_types[i] = content}
+      file_extension.each{|i| @mime_types[i] = content}
     end
-    mime_types
+  end
+
+  def [](file_extension)
+    @mime_types[file_extension]
   end
 end
