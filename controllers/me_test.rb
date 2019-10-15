7330efb4ec658d9902312605a079a0eb894cd6fb
Jason Rhoads <jasrhoads@gmail.com>
2019-10-15 15:30:21 -0700

    cookie data accessed

--- controllers/me_test.rb
+++ controllers/me_test.rb
@@ -1,8 +1,10 @@
 class MeTest
   def me(request, directory, mime_types)
+    cookie_info = request.cookies
     success = File.read("#{directory}/header.txt") + "    
         <main>
         <h1>Mr. Alladin Sir What Will You're Pleasure Be!</h1>
+          <p>#{cookie_info["NomNom"]}</p>
           <img src=\"/images/Genie_Aladdin.png\" alt=\"Genie of the Lamp\">
         </main>
       </body> 
