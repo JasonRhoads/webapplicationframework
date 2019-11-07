07fd193bb001134b5d1e1d2fbd706b7b9c3585df
Jason Rhoads <jasrhoads@gmail.com>
2019-11-07 15:05:08 -0800

    incorporating cookies

--- controllers/me_test.rb
+++ controllers/me_test.rb
@@ -1,10 +1,17 @@
+require_relative '..\cookie'
+
 class MeTest
   def me(request, directory, mime_types)
-    cookie_info = request.cookies
+    cookie = Cookie.new(request)
+    #cookie_info = request.cookies
+    #puts cookie_info
+    #cookie.hash_it
+    #cookie_string = ""
+    #cookie.each
     success = File.read("#{directory}/header.txt") + "    
         <main>
         <h1>Mr. Alladin Sir What Will You're Pleasure Be!</h1>
-          <p>#{cookie_info["NomNom"]}</p>
+          <p>#{cookie.each}</p>
           <img src=\"/images/Genie_Aladdin.png\" alt=\"Genie of the Lamp\">
         </main>
       </body> 
