633adfe71599c604e0320e11ab97660335849666
Jason Rhoads <jasrhoads@gmail.com>
2019-11-26 13:08:18 -0800

    cookie added

--- controllers/login.rb
+++ controllers/login.rb
@@ -1,3 +1,5 @@
+require_relative '..\cookie'
+
 class Login
   def login(request, directory, mime_types)
     database = {
@@ -6,12 +8,16 @@ class Login
       "max" => "cpts",
       "admin" => "password",
     }
-    
     username = request.params["username"]
     password = request.params["password"]
+
+    $cookie["Name"] = username
+    $cookie["NomNom"] = "Choco"
+    $cookie.max_age = 100000
+    
     if ((database.key?(username)) && (password == database[username]))
-      [200, {"Content-type" => "text/html", "class_name" => "Login", "method_name" => "login", "Set-Cookie" => "NomNom=Hello World;"}, [File.read("#{directory}/success.html")]]  
-    else
+      [200, {"Content-type" => "text/html", "class_name" => "Login", "method_name" => "login"}, [File.read("#{directory}/success.html")]]  
+    else  #'_clarity_session={"NomNom": "Hello World", "YumYum": "cookie2"}; Max-Age=100000; Path=\/me;'
       [401, {"Content-type" => "text/html", "class_name" => "Login", "method_name" => "login"}, [File.read("#{directory}/failed.html")]]  
     end
   end
