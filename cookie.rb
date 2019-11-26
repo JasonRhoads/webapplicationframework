cf90d37f4b13445906d65c89b6c563d4702e45ad
Jason Rhoads <jasrhoads@gmail.com>
2019-11-26 13:07:26 -0800

    Update cookie.rb

--- cookie.rb
+++ cookie.rb
@@ -1,85 +1,76 @@
 require "rack/request"
+require "json"
+require "pry"
 
 class Cookie < Rack::Request
   def initialize(request, cookie_string = request.env["HTTP_COOKIE"])
-    @cookie = cookie_string
-    puts @cookie
-    @is_cookie_hash = false 
+    cookie_string.split("=").tap do |c|
+      @name = c[0]
+      #decrypt cookie here
+      @data = JSON.parse(c[1])
+    end
+    @meta_data = {"Path" => "/"}
+    @has_changed = false
   end
     
   def serialize
-    return @cookie if !@is_cookie_hash
-    cookie_string = ""
-    @cookie.each {|key, value| cookie_string += "#{key}=#{value}; "}
-    @is_cookie_hash = false
-    @cookie = cookie_string 
+    cookie_string = "#{@name}=#{@data.to_json}; "
+    @meta_data.each {|key, value| cookie_string += "#{key}=#{value}; "}
+    #encrypt cookie here
+    cookie_string 
   end
 
-  def hash_it
-    return @cookie if @is_cookie_hash
-    cookie_array = @cookie.split("; ")
-    cookie_hash = {}
-    cookie_array.each do |c|
-      key, value = c.split("=").flatten
-      cookie_hash[key] = value
-    end
-    @is_cookie_hash = true
-    @cookie = cookie_hash     
+  # @cookie["foo"] = "bar"
+  def [](key)
+    @data[key]
+  end
+  
+  def []=(key, value)
+    @has_changed = true
+    @data[key] = value
   end
 
-  def add_field(key, value)
-    return @cookie[key] = value if @is_cookie_hash
-    @cookie += "#{key}=#{value}; "
-  end
-
-  def delete_field(key)
-    return @cookie.delete(key) if @is_cookie_hash
-    hash_it
-    delete_field(key)
-    serialize
+  def delete(key)
+    @has_changed = true
+    @data.delete(key)
   end
 
   def each
-    hash_it
-    cookie_string = ""
-    @cookie.each {|key, value| cookie_string += "#{key} = #{value} "}
-    cookie_string
+    @data.each{ |key, value| yield(key, value) }
   end
 
-  def add_max_age(number_of_seconds)
-    add_field("Max-Age", number_of_seconds)
+  def max_age
+    @meta_data["Max-Age"]
   end
 
-  def add_expires(time)
-    add_field("Expires", time)
+  def max_age=(number_of_seconds)
+    @has_changed = true
+    @meta_data["Max-Age"] = number_of_seconds
   end
 
-  def eat_cookie
-    add_max_age(-1)
+  def expires
+    @meta_data["Expires"]
   end
 
-  def delete_cookie
-    eat_cookie
+  def expires=(date)
+    @has_changed = true
+    @meta_data["Expires"] = date
   end
 
-  #cookie = Cookie.new("Nom=Hello World")
-  #puts cookie
-  #puts cookie.serialize
-  #puts cookie.hash_it
-  #cookie.add_field("test", 12345)
-  #puts cookie
-  #puts cookie.serialize
-  #puts cookie.hash_it
-  #cookie.delete_field("test")
-  #puts cookie
-  #puts cookie.serialize
-  #puts cookie.hash_it
-  #puts cookie
-  #puts cookie.serialize
-  #cookie.add_field("test", 12345)
-  #puts cookie.serialize
-  #cookie.delete_field("test")
-  #puts cookie.serialize
-  #puts cookie.hash_it
+  def eat
+    self.max_age = -1
+  end
+
+  def destroy
+    eat
+  end
+
+  def path
+    @meta_data["Path"]
+  end
+  
+  def path=(path)
+    @meta_data["Path"] = path
+  end
   
 end
