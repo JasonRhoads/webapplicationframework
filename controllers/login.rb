f809aa0aa1eddb460250514431e3e5ef052182ea
Jason Rhoads <jasrhoads@gmail.com>
2020-04-06 20:01:18 -0700

    cleaned up login and deleted comments

--- controllers/login.rb
+++ controllers/login.rb
@@ -8,18 +8,19 @@ class Login
   def login(request, directory, mime_types)
     db = Sequel.connect('sqlite://database.db')
 
-    password = db['select password from users where first_name = ?', request.params["username"]].first[:password].chomp
+    password = db['select password from users where first_name = ?', request.params["username"]].first[:password]
     username = db['select first_name from users where first_name = ?', request.params["username"]].first[:first_name]
 
     $cookie["Name"] = username
     $cookie["NomNom"] = "Choco"
     $cookie.max_age = 100000
+
     puts username
     puts password
     puts request.params["password"]
-    #binding.pry
+    
     hashed_password = BCrypt::Password.new(password)
-    puts "Correct password? " + (hashed_password == BCrypt::Password.create("pass1")).to_s
+        
     if ((username == request.params["username"]) && (hashed_password == request.params["password"]))
       [200, {"Content-type" => "text/html", "class_name" => "Login", "method_name" => "login"}, [File.read("#{directory}/success.html")]]  
     else  #'_clarity_session={"NomNom": "Hello World", "YumYum": "cookie2"}; Max-Age=100000; Path=\/me;'
